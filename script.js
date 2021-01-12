a229e7b70dcd2cb8fbb71997b2def93eb5363348
kelvinhovinho <kelvinadamba@hotmail.com>
2021-01-12 17:15:56 +0300

    did changes on html h3

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Order.prototype.fullOrder = function () {
     return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
 };
 
-function Total(price, quantity, delivery) {
+function Total(price, quantity, delivery,)  {
     this.price = price;
     this.quantity = quantity;
     this.delivery = delivery;
