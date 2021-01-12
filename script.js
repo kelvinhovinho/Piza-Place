9d684e149e39797c3ff1a07ff775f5d9e3dd07cf
kelvinhovinho <kelvinadamba@hotmail.com>
2021-01-12 14:36:33 +0300

    add the order functionality in java script

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-// function Order(type, size, crust, topping) {
-//     this.type = type;
-//     this.size = size;
-//     this.crust = crust;
-//     this.topping = topping;
-// }
+function Order(type, size, crust, topping) {
+    this.type = type;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+}
 
-// Order.prototype.fullOrder = function () {
-//     return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
-// };
+Order.prototype.fullOrder = function () {
+    return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
+};
 
-// function Total(price, quantity, delivery) {
-//     this.price = price;
-//     this.quantity = quantity;
-//     this.delivery = delivery;
-// }
+function Total(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+}
 
-// Total.prototype.finalTotal = function () {
-//     return this.price * this.quantity + this.delivery;
-// };
-
-
-// var sizePrice = [1200, 900, 600]
-// var deliverPrices = [0, 200];
-// //user interface logic
-// $(document).ready(function () {
-//     $('form#myform').submit(function (event) {
-//         event.preventDefault();
-//         var pizzaType = $('#type').val();
-
-//         var pizzaSize = parseInt($('#size').val());
-
-//         var pizzaCrust = $('#crust').val();
-
-//         var pizzaTop = $('#top').val();
-
-//         var pizzaQty = parseInt($('#qty').val());
-
-//         var pizzaPick = parseInt($('#pick').val());
+Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+};
 
 
-//         var price = sizePrice[pizzaSize - 1];
+var sizePrice = [1200, 900, 600]
+var deliverPrices = [0, 200];
+//user interface logic
+$(document).ready(function () {
+    $('form#myform').submit(function (event) {
+        event.preventDefault();
+        var pizzaType = $('#type').val();
+
+        var pizzaSize = parseInt($('#size').val());
+
+        var pizzaCrust = $('#crust').val();
+
+        var pizzaTop = $('#top').val();
+
+        var pizzaQty = parseInt($('#qty').val());
+
+        var pizzaPick = parseInt($('#pick').val());
 
 
-//         var DeliveryCost = deliverPrices[pizzaPick - 1];
+        var price = sizePrice[pizzaSize - 1];
+
+
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
 
 
 
-//         newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
-//         newTotal = new Total(price, pizzaQty, DeliveryCost);
-//         if (pizzaPick===1){
-//         alert("Your oder is: " + newOrder.fullOrder() + "your bill is: " + newTotal.finalTotal());
-//         }
-//         else{
-//             if(pizzaPick===2){
-//                 prompt("Enter where you want your pizza to be delivered");
-//                 alert("Your order has been received and it will be delivered. Continue to see your order details");
-//                 alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
-//                 alert("your bill is: " + newTotal.finalTotal());
-//             }
-//         }
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
+        alert("Your oder is: " + newOrder.fullOrder() + "your bill is: " + newTotal.finalTotal());
+        }
+        else{
+            if(pizzaPick===2){
+                prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal());
+            }
+        }
 
-//     });
-// });
+    });
+});
